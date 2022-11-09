44b7797be49f0df92991917dbca392e631708dfd
dbconsultoria <dbconsultoria@gmail.com>
2022-11-09 08:20:38 -0300

    Key

--- pyreadweatherapi.py
+++ pyreadweatherapi.py
@@ -1,7 +1,7 @@
 import requests
 import json
 
-api_key = "7edc3a522309c1d3d79acde91f0294a7"
+api_key = ""
 lat = "48.208176"
 lon = "16.373819"
 url = "https://api.openweathermap.org/data/2.5/onecall?lat=%s&lon=%s&appid=%s&units=metric" % (lat, lon, api_key)
