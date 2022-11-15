a4e4ac0b9ac920ba9500264a76b39307775c025c
dbconsultoria <dbconsultoria@gmail.com>
2022-11-15 08:00:28 -0300

    Desvio e lambda

---
+++ pylambda.py
@@ -0,0 +1,4 @@
+comissao = lambda a : a * 0.3
+
+print(comissao(1000))
+print(comissao(2000))
---
+++ pystandarddeviation.py
@@ -0,0 +1,8 @@
+#https://www.w3schools.com/python/python_ml_standard_deviation.asp
+import numpy
+
+speed = [86,87,88,86,87,85,86]
+
+x = numpy.std(speed)
+
+print(x)
